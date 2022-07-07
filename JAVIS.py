5c0b65ae3a067deb7fc1d86650bc7380a81c434b
Okenwa Bright <iisavage684@gmail.com>
2022-07-07 10:37:11 +0100

    Update JAVIS.py

--- JAVIS.py
+++ JAVIS.py
@@ -2,7 +2,7 @@ import os,time,rich
 from rich.panel import Panel as nel
 from rich import print as cetak
  
-__alvino__ganteng__ = '\t[yellow]•[white] I Am Mr.[green] The[white] Young[yellow] •[white]'
+__alvino__ganteng__ = '\t[yellow]•[white] I Am [green] The ONLY[white] OKENWA🇳🇬[yellow] •[white]'
 cetak(nel(__alvino__ganteng__));time.sleep(1)
  
 if __name__ == "__main__":
