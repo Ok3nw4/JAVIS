275e759701626386581e3a84ad696a9ed486b9b5
Okenwa Bright <iisavage684@gmail.com>
2022-07-07 09:58:07 +0100

    Update JAVIS.py

--- JAVIS.py
+++ JAVIS.py
@@ -1,10 +1,4 @@
-print ("\033[0;93mCreated By NWABUKING && JEWEL CYBER TECH ")
+print ("\033[0;93mCreated by  okenwa&& JEWEL CYBER TECH ")
 print ("\033[0;92mFuckyou")
-import dc
-dc.menu()
-
-if __name__ == "__main__":
-        try:
-                __import__("JAVIS").login()
-        except Exception as e:
-                exit(str(e))
+import JAVIS
+JAVIS.menu()
